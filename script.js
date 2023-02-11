7515ede106240f19253260db898749df1b7078a6
urc4 <121357043+urc4@users.noreply.github.com>
2023-02-11 16:17:24 -0300

    Add listeners for numbers
    Updates the display with the number selected, still gotta watch out
    for when adding clear and delete buttons.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const CALCULATOR = document.querySelector(".calculator");
+const CALC_DISPLAY = CALCULATOR.querySelector(".display");
+const CALC_BUTTONS = CALCULATOR.querySelector(".btns");
+
 const Operators = {
   add: (augend, addend) => augend + addend,
   subtract: (minuend, subtrahend) => minuend - subtrahend,
@@ -28,10 +32,53 @@ function operate(operator, firstOperand, secondOperand) {
     case "+/-":
       return Operators.negate(firstOperand);
       break;
+    default:
+      break;
   }
 }
+
+function updateDisplayHistory(current, operator) {
+  const calcHistory = CALC_DISPLAY.querySelector(".history");
+}
+
+// function updateDisplayCurrent(digit) {
+//   const currentNumber = CALC_DISPLAY.querySelector(".current");
+//   if (currentNumber.textContent === "0") currentNumber.textContent = `${digit}`;
+//   else currentNumber.textContent += `${digit}`;
+// }
+
+const Display = {
+  updateDisplayHistory: updateDisplayHistory,
+  updateDisplayCurrent: (digit) => {
+    const currentNumber = CALC_DISPLAY.querySelector(".current");
+    if (currentNumber.textContent === "0")
+      currentNumber.textContent = `${digit}`;
+    else if (typeof Number(currentNumber.textContent) === "number")
+      currentNumber.textContent += `${digit}`;
+  },
+};
+
+// add event listerns for keys use algo + button.id
+// can i not use this.numberBtns here?
+
+const NumberButtons = {
+  numberBtns: CALC_BUTTONS.querySelectorAll(".number"),
+  Display: Display,
+  activateNumberListeners: () => {
+    NumberButtons.numberBtns.forEach((btn) => {
+      btn.addEventListener("mousedown", (event) => {
+        // add if statement whether it has reached maximum display size
+        Display.updateDisplayCurrent(event.target.id);
+      });
+    });
+  },
+};
+
+NumberButtons.activateNumberListeners();
 
 const Calculator = {
   Operators: Operators,
   operate: operate,
+  updateDisplay: null,
+  currentValue: null,
 };
